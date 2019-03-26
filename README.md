64f1aa929ea9c0071eb10da7e41319171bb57091
Tomasz Martyński <tomek@martynski.pl>
2019-03-26 17:10:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## How to run
+
+```bash
+git clone git@github.com:elderapo/babel-7-property-decorator-issue.git
+cd babel-7-property-decorator-issue
+yarn install
+yarn ts-node-start/babel-start # to see the difference in the output
+yarn build # to see compiled code differences (babel vs tsc)
+```
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+const LogUpdate = () => {
+  return (target: Object, key: string | symbol) => {
+    let val = target[key];
+
+    const getter = () => {
+      return val;
+    };
+
+    const setter = next => {
+      console.log(`Updating: ${val} => ${next}...`);
+      val = next;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true
+    });
+  };
+};
+
+class Something {
+  @LogUpdate()
+  public count: number = 0;
+}
+
+const something = new Something();
+something.count++;
+something.count = 123;
+something.count = -1;
